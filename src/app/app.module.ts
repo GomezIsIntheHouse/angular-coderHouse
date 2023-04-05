717a8a76550ab3c641c1be484e00c4e1bebcef7e
julian <julian1995ag@gmail.com>
2023-04-05 16:31:45 -0300

    avances en entrega n° 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {FormsModule, ReactiveFormsModule} from '@angular/forms' //paso 1
 // import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
@@ -11,6 +11,7 @@ import { AboutComponent } from './components/about/about.component';
 // Rutas
 import { APP_ROUTING } from './app.routes';
 import { AsideComponent } from './components/shared/aside/aside.component';
+import { ReactiveFormComponent } from './components/reactive-form/reactive-form.component';
 
 @NgModule({
   declarations: [
@@ -20,12 +21,15 @@ import { AsideComponent } from './components/shared/aside/aside.component';
     HeroesComponent,
     AboutComponent,
     AsideComponent,
+    ReactiveFormComponent,
     
   ],
   imports: [
     BrowserModule,
     // AppRoutingModule,
-    APP_ROUTING
+    APP_ROUTING,
+    ReactiveFormsModule,
+    FormsModule
 
   ],
   providers: [],
---
+++ src/app/components/reactive-form/reactive-form.component.ts
@@ -0,0 +1,102 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+@Component({
+  selector: 'app-reactive-form',
+  templateUrl: './reactive-form.component.html',
+  styleUrls: ['./reactive-form.component.scss']
+})
+export class ReactiveFormComponent implements OnInit {
+  forma= new FormGroup({});
+
+  constructor(private fb:FormBuilder) { }
+
+  ngOnInit(): void {
+    this.crearFormulario();
+    this.cargarDataAlFormulario();
+  }
+
+  get nombreNoValido(){
+    return this.forma.get('nombre')?.invalid && this.forma.get('nombre')?.touched
+  }
+
+  get apellidoNoValido(){
+    return this.forma.get('apellido')?.invalid && this.forma.get('apellido')?.touched
+  }
+
+  get correoNoValido(){
+    return this.forma.get('correo')?.invalid && this.forma.get('correo')?.touched
+  }
+  get distritoNoValido(){
+    return this.forma.get('direccion.distrito')?.invalid && this.forma.get('direccion.distrito')?.touched //complemento paso 4
+  }
+  get ciudadNoValido(){
+    return this.forma.get('direccion.ciudad')?.invalid && this.forma.get('direccion.ciudad')?.touched
+  }
+
+  crearFormulario (){   //paso 3
+    this.forma = this.fb.group({
+      nombre:['', [Validators.required, Validators.minLength(5)] ], //validador asincrono, se ejecuta en el mismo hilo de tiempo.
+      apellido:['', Validators.required],
+      correo:['', [ Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{3}$')]], //el pattern se utiliza para validaciones mediante el uso de expresiones regulares.
+  
+      direccion: this.fb.group({
+  
+        distrito:['', Validators.required],
+        ciudad:['', Validators.required],
+      })
+    })
+  
+  
+    }
+    cargarDataAlFormulario(){
+      //con setValue debo setearle un valor x defecto a todos los campos
+      this.forma.setValue({
+    
+          nombre: 'Juanito',
+          apellido: 'Perez',
+          correo: 'juan@gmail.com',
+          direccion: {
+            distrito: 'Ontario',
+            ciudad: 'Otawa'
+          }
+    
+      })
+      //para evitar el inconveniente de tener un valor de campo no seteado se utilizar el RESET
+    //   this.forma.reset({
+    
+    //     nombre: ,
+    //     apellido: 'Perez',
+    //     correo: 'juan@gmail.com',
+    //     direccion: {
+    
+    //     }
+    
+    // })
+    }
+    
+    
+      guardar () {
+        console.log(this.forma.value)    
+        if(this.forma.invalid){
+    
+          return Object.values(this.forma.controls).forEach(control =>{
+    
+            //paso 5 pregunto si es de un Form Group o de un Control
+            //Group para realizar el touched a la direccion
+            if (control instanceof FormGroup){
+              Object.values(control.controls).forEach(control => control.markAsTouched());
+            }else{
+            control.markAsTouched();
+            }
+    
+          })
+          console.log(this.forma);
+    
+        }
+    
+        //posteo de la informacion - paso 7
+        //resetear los campos del formulario - paso 8
+        this.forma.reset();
+      }
+
+}
